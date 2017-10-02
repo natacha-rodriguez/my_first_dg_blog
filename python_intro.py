e42992885b06b3066c8cf4cc996b34a9f586c4d2
Naty <natacha.v.rodriguez.a@gmail.com>
2017-10-02 20:31:57 -0300

    Mi app de Django girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,15 @@
+
+
+def hi(name):
+    print('Hi '+ name + '!')
+    
+
+girls = ['Natacha', 'Ileana', 'Ivana']    
+
+for name in girls:
+    hi(name)
+    print('Next...')
+    
+for i in range(1,9):
+    print(i)
+    
